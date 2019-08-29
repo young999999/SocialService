aae8922fdf06028a0182df258c63a9a88e1234be
young999999 <1972780113@qq.com>
2019-08-29 22:32:15 +0800

    添加问答、招聘、文章模块

---
+++ recruit/src/main/java/com/py/recruit/dao/EnterpriseDao.java
@@ -0,0 +1,19 @@
+package com.py.recruit.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+import com.py.recruit.pojo.Enterprise;
+
+import java.util.List;
+
+/**
+ * 数据访问接口
+ * @author Administrator
+ *
+ */
+public interface EnterpriseDao extends JpaRepository<Enterprise,String>,JpaSpecificationExecutor<Enterprise>{
+
+    public List<Enterprise> findByIshot(String ishot);//where ishot=?
+	
+}
