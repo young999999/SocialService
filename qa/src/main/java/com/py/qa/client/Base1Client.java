88e3aee7cdd161bebb608c829e292c2eedda1199
young999999 <1972780113@qq.com>
2019-09-05 17:47:32 +0800

    加入网关zuul、集中配置组件config、消息总线组件bus

--- qa/src/main/java/com/py/qa/client/Base1Client.java
+++ qa/src/main/java/com/py/qa/client/Base1Client.java
@@ -2,6 +2,7 @@ package com.py.qa.client;
 
 import entity.Result;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
